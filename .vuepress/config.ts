bdcb8939a79eb2265fcd3e7d9dc729eea46ff794
mahula <lenzmath@posteo.de>
2024-02-20 21:57:13 +0100

    add vuepress for documentation

---
+++ .vuepress/config.ts
@@ -0,0 +1,53 @@
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { hopeTheme } from 'vuepress-theme-hope'
+
+export default defineUserConfig({
+  // meta
+  title: 'IT4C - E2E Cypress Cucumber Boilerplate Documentation',
+  description: 'IT4C - E2E Cypress Cucumber Boilerplate Documentation',
+  dest: 'build/docs',
+  base: process.env.VUEPRESS_BASE ? `/${process.env.VUEPRESS_BASE}/` : '/',
+  pagePatterns: ['**/*.md', '**/LICENSE', '!.vuepress', '!node_modules'],  
+  // bundler
+  bundler: viteBundler({}),
+  // theme
+  theme: hopeTheme({
+    favicon: 'favicon.ico',
+    logo: '/it4c-logo2-clean-bg_alpha-128x128.png',
+    contributors: false,
+    docsBranch: 'master',
+    docsDir: '.',
+    docsRepo: 'https://github.com/IT4Change/boilerplate-e2e-cypress-cucumber',
+    editLink: true,
+    lastUpdated: false,
+    print: false,
+    repo: 'IT4Change/boilerplate-e2e-cypress-cucumber',
+    repoLabel: 'GitHub',
+    repoDisplay: true,
+    navbarLayout: {
+      start: ['Brand'],
+      end: ['Repo', 'Outlook', 'Search'],
+    },
+    plugins: {
+      mdEnhance: {
+        tabs: true,
+        imgSize: true
+      },
+      searchPro: {
+        indexContent: true,
+        autoSuggestions: true,
+        customFields: [
+          {
+            getter: (page) => page.frontmatter.category,
+            formatter: "Category: $content",
+          },
+          {
+            getter: (page) => page.frontmatter.tag,
+            formatter: "Tag: $content",
+          },
+        ],
+      }
+    }
+  }),
+})
