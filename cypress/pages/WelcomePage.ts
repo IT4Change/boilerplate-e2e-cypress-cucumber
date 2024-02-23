4f54062882db770c8128ef1b67d6be19b3f61f6d
mahula <lenzmath@posteo.de>
2024-02-23 19:45:37 +0100

    make test fail for testing

--- cypress/pages/WelcomePage.ts
+++ cypress/pages/WelcomePage.ts
@@ -1,7 +1,7 @@
 class WelcomePage {
   successMessage: string = '.flash.success'
   WelcomeHeader: string = '.subheader'
-  logoutBtn: string = 'a[href="/logout"]'
+  logoutBtn: string = 'a[href="/logoutTEST"]'
 }
 
 export const welcomePage = new WelcomePage()
