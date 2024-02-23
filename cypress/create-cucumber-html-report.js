3b6ebf591c4a8ceadd4f635adc7b02742cae7799
mahula <lenzmath@posteo.de>
2024-02-23 19:58:49 +0100

    configure html reporting for failing test run

--- cypress/create-cucumber-html-report.js
+++ cypress/create-cucumber-html-report.js
@@ -3,7 +3,7 @@ import report from 'multiple-cucumber-html-reporter'
 const reportTitle = 'DreamMall End-to-End Test Report'
 
 report.generate({
-  jsonDir: 'reports/json_logs',
+  jsonDir: './reports/json/',
   reportPath: './reports/e2e_html_report',
   pageTitle: reportTitle,
   reportName: reportTitle,
