74d5511cfa7db12c1dd808aa02458d088cacff94
mahula <lenzmath@posteo.de>
2025-05-04 22:35:05 +0200

    eslint: fix linting

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,5 +1,5 @@
-import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from 'vuepress'
 import { hopeTheme } from 'vuepress-theme-hope'
 
 export default defineUserConfig({
